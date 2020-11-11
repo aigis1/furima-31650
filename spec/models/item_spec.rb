07c481010c81209e809227ef9faf5dca5209036a
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-11 12:38:26 +0900

    ActiveHashの項目がid:1では保存できないことのテストコードの記述
    prefectureに関してはid: 1が北海道なので無し

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -59,6 +59,30 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Scheduled delivery Select")
     end
 
+    it 'カテゴリーの情報が---では保存できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it '商品の状態についての情報が---では保存できないこと' do
+      @item.sales_status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status Select")
+    end
+
+    it '配送料の負担についての情報が---では保存できないこと' do
+      @item.shipping_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee Select")
+    end
+
+    it '発送までの日数についての情報が---では保存できないこと' do
+      @item.scheduled_delivery_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Scheduled delivery Select")
+    end
+
     it '価格についての情報が必須であること' do
       @item.price = nil
       @item.valid?
