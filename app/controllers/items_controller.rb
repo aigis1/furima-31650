9fb18f158b8afcce7e3e4744d21788c50941e4a8
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-12 11:42:37 +0900

    destroy, updateアクション内の記述を最適化
    set_itemの対象にupdateアクションを追加し、記述を簡略化
    destroyアクションのif文内で行っていたmove_to_indexをbefore_actionでの処理に変更した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order(id: "DESC")
@@ -28,7 +28,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,8 +40,6 @@ class ItemsController < ApplicationController
     if current_user == @item.user
       @item.destroy
       redirect_to root_path
-    else
-      move_to_index
     end
   end
 
