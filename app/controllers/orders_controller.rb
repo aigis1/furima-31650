f15b87bd2698fba44bb91eb1373a4c9ac09efeab
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-13 12:37:44 +0900

    order_addressモデルの作成と住所情報、購入情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,24 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if@order_address.valid?
+      @order_address.save
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, 
+                :city, :house_number, :building, :telephone).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
 end
