e7a15c4c6804dbd560fda78576f9121f9ac8c0a7
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-14 21:37:08 +0900

    エラーメッセージの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "must select" }
     validates :city
     validates :house_number
     validates :telephone, format: {with: /\A\d{10,11}\z/, message: "number Input only number"}
