7d5c181ea4f421c07303a08fa35e080e182e8818
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-14 18:28:45 +0900

    Payjpによる決済機能の導入

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number,
-                :building, :telephone, :item_id, :user_id
+                :building, :telephone, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -9,6 +9,7 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :telephone, format: {with: /\A\d{10,11}\z/, message: "number Input only number"}
+    validates :token
   end
 
   def save
